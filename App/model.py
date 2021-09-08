4b6473ed3fdf0b3c270724dc707120eff2ccbf33
Maria Alejandra Moreno Bustillo <m.morenob@uniandes.edu.co>
2021-09-08 11:35:44 -0500

    requerimiento 1 listo

--- App/model.py
+++ App/model.py
@@ -27,7 +27,7 @@
 
 import config as cf
 from DISClib.ADT import list as lt
-from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.Algorithms.Sorting import mergesort as sa
 assert cf
 
 """
@@ -39,10 +39,9 @@ los mismos.
 # Construccion de modelos
 def newCatalog(list_type = 'ARRAY_LIST'):
     """
-    Inicializa el catálogo de libros. Crea una lista vacia para guardar
-    todos los libros, adicionalmente, crea una lista vacia para los autores,
-    una lista vacia para los generos y una lista vacia para la asociación
-    generos y libros. Retorna el catalogo inicializado.
+    Inicializa el catálogo de artistas y obras. Crea una lista vacia para guardar
+    todas las obras y artistas del museo, adicionalmente se crea una lista que relaciona las obras de arte con sus artistas y una lista
+    que relaciona a los artistas con su fecha de nacimiento. Retorna el catalogo inicializado.
     """
     catalog = {'Artwork': None,
                'Artist': None,
@@ -66,40 +65,46 @@ def newCatalog(list_type = 'ARRAY_LIST'):
 
 def addArtwork(catalog, artwork):
 
-    list_artwork = {'ObjectID':artwork['ObjectID'], 'Title':artwork['Title'], 'ConstituentID':artwork['ConstituentID'], 'Date': artwork[ 'Date'],
-     'Medium':artwork['Medium'], 'Dimensions':artwork['Dimensions'],'CreditLine': artwork['CreditLine'], 'Department':artwork['Department'], 'DateAcquired':artwork['DateAcquired']  }
+    "Se utiliza un diccionario para extraer únicamente los datos necesarios"
+
+    list_artwork = {'ObjectID':artwork['ObjectID'], 
+                    'Title':artwork['Title'], 
+                    'ConstituentID':artwork['ConstituentID'], 
+                    'Date': artwork[ 'Date'],
+                    'Medium':artwork['Medium'], 
+                    'Dimensions':artwork['Dimensions'],
+                    'CreditLine': artwork['CreditLine'], 
+                    'Department':artwork['Department'], 
+                    'DateAcquired':artwork['DateAcquired']}
 
     lt.addLast(catalog['Artwork'], list_artwork)
     
     artist_id = artwork['ConstituentID'].split(',')
 
-    #for artist in artist_id:
-        #addArtworkArtist(catalog, artist.strip(), artwork)
+    for artist in artist_id:
+        addArtworkArtist(catalog, artist.strip(), artwork)
 
 
 def addArtist(catalog,artists):
-    list_artist = {'ConstituentID':artists['ConstituentID'], 'DisplayName': artists['DisplayName'], 'Nationality':artists['Nationality'],
-    'Gender':artists['Gender'], 'BeginDate':artists['BeginDate'], 'EndDate':artists['EndDate']}
+    artist = {'ConstituentID':artists['ConstituentID'],
+                    'DisplayName': artists['DisplayName'],
+                    'Nationality':artists['Nationality'],
+                    'Gender':artists['Gender'],
+                    'BeginDate':artists['BeginDate'],
+                    'EndDate':artists['EndDate'],
+                    'Artworks':lt.newList()}
+                    
+    addArtistDate(catalog, artist['DisplayName'], artist['BeginDate'],artist['EndDate'],artist['Nationality'],artist['Gender'])
     
-    addArtistDate(catalog, list_artist['DisplayName'], list_artist['BeginDate'],list_artist['EndDate'],list_artist['Nationality'],list_artist['Gender'])
-
-
-    lt.addLast(catalog['Artist'], list_artist)  
+    lt.addLast(catalog['Artist'], artist)  
     
-    
-
-
         
-#def addArtworkArtist(catalog, artist_id, artwork):
-   # artists = catalog['ArtworkArtist']
-    #posartist = lt.isPresent(artists, artist_id)
-    #if posartist > 0:
-    #    artist = lt.getElement(artists, posartist)
-   # else:
-     #   artist = newArtist(artist_id)
-    #  lt.addLast(artists, artist)
-    #lt.addLast(artist['ArtworkArtist'], artwork)
-
+def addArtworkArtist(catalog, artist_id, artwork):
+    artists = catalog['Artist']
+    posartist = lt.isPresent(artists, artist_id)
+    if posartist > 0:
+        artist = lt.getElement(artists, posartist)
+    lt.addLast(artist['Artworks'], artwork)
 
 def addArtistDate(catalog, artist, date, deathdate, nationality, gender):
     
@@ -111,13 +116,6 @@ def addArtistDate(catalog, artist, date, deathdate, nationality, gender):
 
 # Funciones para creacion de datos
 
-def newArtist(name):
-
-    artist = {'Artist_ID':'','Artworks':None,}
-    artist['Artist_ID'] = name
-    artist['Artworks'] = lt.newList('ARRAY_LIST')
-
-    return artist
 
 def newArtistDate(artist, date, deathdate, nationality, gender):
     artist_date = {'name': '', 'BeginDate':'', 'EndDate':'', 'nationality':'','gender':''}
@@ -132,21 +130,23 @@ def newArtistDate(artist, date, deathdate, nationality, gender):
 # Funciones de consulta
 def getArtistYear(catalog,año_inicial,año_final):
 
-    artist_inrange = lt.newList()
+    sortYear(catalog)
+
+    artist_inrange = lt.newList("ARRAY_LIST")
 
     for artist in lt.iterator(catalog['ArtistDate']):
 
         if int(artist['BeginDate']) >= año_inicial and int(artist['BeginDate']) <= año_final:
         
             lt.addLast(artist_inrange, artist )
-    
+
     return artist_inrange
 
 # Funciones utilizadas para comparar elementos dentro de una lista
 
 def cmpartistyear(artist1,artist2):
 
-    return int(artist1['date']) > int(artist2['date'])
+    return int(artist1['BeginDate']) < int(artist2['BeginDate'])
 
 # Funciones de ordenamiento
 
